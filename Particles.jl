25393d134cb011fa9046fb2d675f49a7224fe240
Leandro Martinez <leandro@iqm.unicamp.br>
2022-07-29 14:02:52 -0300

    up

--- Particles.jl
+++ Particles.jl
@@ -6,46 +6,60 @@ include("./WithAgents.jl")
 include("./NoAgents.jl")
 include("./OnlyAgents.jl")
 
+function compare(n, nsteps)
+    println("With Agents.jl")
+    WithAgents.simulate(nsteps=1, n=n); # compile
+    @time WithAgents.simulate(nsteps=nsteps, n=n);
+    
+    println("No Agents.jl")
+    NoAgents.simulate(nsteps=1, n=n); # compile
+    @time NoAgents.simulate(nsteps=nsteps, n=n);
+    
+    println("Only Agents.jl")
+    OnlyAgents.simulate(nsteps=1, n=n); # compile
+    @time OnlyAgents.simulate(nsteps=nsteps, n=n);
+    
+    println("With Agents.jl, only stepping")
+    println(" serial: ")
+    model = WithAgents.initialize_model(n=n, parallel=false)
+    WithAgents.simulate(model=model, nsteps=1); # compile
+    @time WithAgents.simulate(model=model, nsteps=nsteps);
+    println(" parallel: ")
+    model = WithAgents.initialize_model(n=n, parallel=true)
+    WithAgents.simulate(model=model, nsteps=1); # compile
+    @time WithAgents.simulate(model=model, nsteps=nsteps);
+    
+    println("No Agents.jl, only stepping")
+    println(" serial: ")
+    system = NoAgents.initialize_system(n=n, parallel=false)
+    NoAgents.simulate(system=system, nsteps=1); # compile
+    @time NoAgents.simulate(system=system, nsteps=nsteps);
+    println(" parallel: ")
+    system = NoAgents.initialize_system(n=n, parallel=true)
+    NoAgents.simulate(system=system, nsteps=1); # compile
+    @time NoAgents.simulate(system=system, nsteps=nsteps);
+    
+    println("Only Agents.jl, only stepping")
+    model = OnlyAgents.initialize_model(n=n)
+    OnlyAgents.simulate(model=model, nsteps=1); # compile
+    @time OnlyAgents.simulate(model=model, nsteps=nsteps);
+
+    nothing
+end
+
 n = 1_000
+nsteps = 1000
+println("-------------------------")
+println(" n = $n ")
+println(" nsteps = $nsteps ")
+println("-------------------------")
+compare(n, nsteps)
+
+
+n = 10_000
 nsteps = 50
-
-# as far as I can tell, the module `Particles` offers nothing.
-# I skip it and add here the benchmarking code immediatelly.
-println("With Agents.jl")
-WithAgents.simulate(nsteps=1, n=n); # compile
-@time WithAgents.simulate(nsteps=nsteps, n=n);
-
-println("No Agents.jl")
-NoAgents.simulate(nsteps=1, n=n); # compile
-@time NoAgents.simulate(nsteps=nsteps, n=n);
-
-println("Only Agents.jl")
-OnlyAgents.simulate(nsteps=1, n=n); # compile
-@time OnlyAgents.simulate(nsteps=nsteps, n=n);
-
-println("With Agents.jl, only stepping")
-println(" serial: ")
-model = WithAgents.initialize_model(parallel=false)
-WithAgents.simulate(model=model, nsteps=1, n=n); # compile
-@time WithAgents.simulate(model=model, nsteps=nsteps, n=n);
-println(" parallel: ")
-model = WithAgents.initialize_model(parallel=true)
-WithAgents.simulate(model=model, nsteps=1, n=n); # compile
-@time WithAgents.simulate(model=model, nsteps=nsteps, n=n);
-
-println("No Agents.jl, only stepping")
-println(" serial: ")
-system = NoAgents.initialize_system(parallel=false)
-NoAgents.simulate(system=system, nsteps=1, n=n); # compile
-@time NoAgents.simulate(system=system, nsteps=nsteps, n=n);
-println(" parallel: ")
-system = NoAgents.initialize_system(parallel=true)
-NoAgents.simulate(system=system, nsteps=1, n=n); # compile
-@time NoAgents.simulate(system=system, nsteps=nsteps, n=n);
-
-println("Only Agents.jl, only stepping")
-model = OnlyAgents.initialize_model()
-OnlyAgents.simulate(model=model, nsteps=1, n=n); # compile
-@time OnlyAgents.simulate(model=model, nsteps=nsteps, n=n);
-
-
+println("-------------------------")
+println(" n = $n ")
+println(" nsteps = $nsteps ")
+println("-------------------------")
+compare(n, nsteps)
